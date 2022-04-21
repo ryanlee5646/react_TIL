1ed6ccde17eb11653d075332df841471772d0a3c
Gyujin Lee <ryanlee5646@gmail.com>
2022-04-21 17:56:05 +0900

    20220421 React_TIL useEffect

--- myApp/react-for-beginners/src/App.js
+++ myApp/react-for-beginners/src/App.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './Button';
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('render');
+  console.log('계속 실행!!');
   // 리렌더링 되더라도 해당 코드가 실행되지 않게하기
+  useEffect(() => {
+    console.log('CALL THE API');
+  }, []);
   return (
     <div className="App">
       <h1>{counter}</h1>
