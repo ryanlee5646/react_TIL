91b14c3a83417f10491c1747e178498b43f0bcba
ryanlee <ryanlee5646@gmail.com>
2022-05-02 00:16:54 +0900

    20220502 react_til

--- myApp/react-for-beginners/src/App.js
+++ myApp/react-for-beginners/src/App.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true); // 로딩 메세지를 보여주기 위함
+  useEffect(() => {
+
+  }, []);
   return (
     <div>
       <h1>The Coins!</h1>
